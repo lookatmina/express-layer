632683b1577aa0247f2b5bd4db6282eed6e240ea
lookatmina <mina@Mina-MacBook-Pro.local>
2024-02-24 00:56:21 +0900

    task: service에서 repository로 넘어가야하는 코드 표시

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -17,7 +17,8 @@ export default class UserService {
     if (!name) {
       throw new Error('name은 필수 입력값입니다.')
     }
-         
+    
+    /////////////////////////////////////////// 프리즈마를 사용하는 부분은 레포지토리로 넘김
     // eamil 유효성 검사 (중복이 있으면 안됨)
     const alreadyUser = await prisma.users.findFirst({
       where: {
@@ -25,12 +26,14 @@ export default class UserService {
       }
     });
     // prisma, @prisma/client 설치 후 명령어 실행: yarn prisma init
-    
+    /////////////////////////////////////////// 여기까지
+
     // user가 존재하면 email이 중복되는 것이기 때문에 error 발생
     if (alreadyUser) {
       throw new Error('존재하는 email입니다.')
     }
-        
+    
+    /////////////////////////////////////////// 프리즈마를 사용하는 부분은 레포지토리로 넘김
     // user table에 user를 create한다.
     const user = await prisma.users.create({
       data: {
@@ -39,6 +42,7 @@ export default class UserService {
         name
       }
     })
+    /////////////////////////////////////////// 여기까지
 
     return user
   };

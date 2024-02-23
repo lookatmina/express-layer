c4fe382166f37f8c1016e75327be33613449ce13
lookatmina <mina@Mina-MacBook-Pro.local>
2024-02-24 00:12:58 +0900

    task: 미들웨어도 try catch 에러처리 추가

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,15 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export default async function (req, res, next) {
-  console.log('auth middleware 입니다.')
-  // access token으로 userId를 확인한다.
-  const accessToken = req.cookies.accessToken;
-  const tokenData = jwt.verify(accessToken, process.env.JWT_SECRET);
-  
-  // req.user에 값을 넣어줌
-  req.user = {
-    userId: tokenData.userId
+  try {
+    console.log('auth middleware 입니다.')
+    // access token으로 userId를 확인한다.
+    const accessToken = req.cookies.accessToken;
+    const tokenData = jwt.verify(accessToken, process.env.JWT_SECRET);
+    
+    // req.user에 값을 넣어줌
+    req.user = {
+      userId: tokenData.userId
+    }
+    
+    next();
+  } catch (e) {
+    return res.status(500).send({
+      message: "예기치 못한 서버 에러 발생"
+    })
   }
-  
-  next()
 }

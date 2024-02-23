2957c570aebaf6a2a3b2fffff05dfd67a5f9c8bb
Minah Yun <mina@Mina-MacBook-Pro.local>
2024-02-23 22:00:40 +0900

    task: 회원가입, 로그인, 프로필 조회 api 생성

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ app.get("/", (req, res) =>
   })
 );
 
+// 회원가입
+app.post("/users/signup", (req, res) =>
+  res.status(200).send({
+    message: "회원가입 api 입니다.",
+  })
+);
+
+// 로그인
+app.post("/users/signin", (req, res) =>
+  res.status(200).send({
+    message: "로그인 api 입니다.",
+  })
+);
+
+// 프로필 조회
+app.get("/users/me", (req, res) =>
+  res.status(200).send({
+    message: "프로필 조회 api 입니다.",
+  })
+);
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on PORT ${port}`);

a8b498c0cd086e14f16d2750c59396d1b1963eb5
lookatmina <mina@Mina-MacBook-Pro.local>
2024-02-23 22:15:44 +0900

    task: url 최상위 도메인 설정

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.json());
 
 // users.router.js로 뺀 라우터들을 불러와서 연결만 해준다.
-app.use("/", [UsersRouter]);
+// 서버 최상위 도메인을 api로 설정하고 싶다. / => /api
+// /users/signup => /api/users/signup 이렇게 변경됨
+app.use("/api", [UsersRouter]);
 
 app.get("/", (req, res) =>
   res.status(200).send({

d56e67290d5f54e9e2ec22764d2d163ed6f945db
lookatmina <mina@Mina-MacBook-Pro.local>
2024-02-23 23:43:45 +0900

    chore: cookie-parser 설치

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import UsersRouter from "./src/routes/users.router.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // users.router.js로 뺀 라우터들을 불러와서 연결만 해준다.
 // 서버 최상위 도메인을 api로 설정하고 싶다. / => /api
